ba29897e1e3a1fb7ea4482d96c84b99007e2291e
Matthew Tan <128443789+matthewtan01@users.noreply.github.com>
2024-06-03 16:34:45 +0800

    complete update user functionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,8 +3,10 @@ import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
 import { app } from "../firebase";
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from 'react-router-dom';
 
 export default function () {
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     const handleGoogleClick = async () => {
         try{
@@ -25,6 +27,7 @@ export default function () {
             const data = await res.json();
             console.log(data);
             dispatch(signInSuccess(data));
+            navigate('/')
         } catch (error){
             console.log("could not login with google", error)
         }
